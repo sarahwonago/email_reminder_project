f2726137ae743bbf8d546e958e27043ce983f0ab
sarahwonago <sarahshaaleh@gmail.com>
2025-09-04 10:53:12 +0300

    running locally

--- email_reminder_project/celery.py
+++ email_reminder_project/celery.py
@@ -1,8 +1,13 @@
 import os
-from email_reminder_project.celery import Celery
+from celery import Celery
 
+# set default Django settings for Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "email_reminder_project.settings")
 
 app = Celery("email_reminder_project")
+
+# Load task settings from Django settings with 'CELERY_' namespace
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Auto-discover tasks in all installed apps
 app.autodiscover_tasks()
